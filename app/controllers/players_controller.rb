5f5d9af918007d46aecfc62eff4503084acb88bd
Patrick <patrickwiechert@yahoo.com>
2016-04-16 22:58:12 -0500

    updated

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,1 +1,60 @@
-#
+### PLAYERS ###
+
+# INDEX
+get "/players" do
+  @players = Player.all
+  erb :"players/index"
+end
+
+
+# NEW
+get "/players/new" do
+  @player = Player.new
+  erb :"players/new"
+end
+
+
+# CREATE
+post "/players" do
+  @player = Player.new(params)
+  if @player.save
+    redirect to('/players')
+  else
+    erb :"players/new"
+  end
+end
+
+
+# SHOW
+get "/players/:id" do
+  @player = Player.find_by_id(params['id'])
+erb :"players/show"
+end
+
+
+# EDIT
+get "/players/:id/edit" do
+  @player = Player.find_by_id(params['id'])
+  @players = Player.all
+  erb :"players/edit"
+end
+
+
+# UPDATE
+patch "/players/:id" do
+  @player = Player.find_by_id(params['id'])
+  if @player.update_attributes(name: params['name'], value: params['value'])
+    redirect to("/players/#{@player.id}")
+  else
+    erb :"players/edit"
+  end
+end
+
+
+# DELETE
+# check scratch for delete
+# put in code for reassign_all_clubs
+#  @player.reassign_all_clubs(@new_player)
+
+
+# ALL CLUBS FOR A SPONSOR
